208e2abf9646e8db2fdf5c1a02ac0bfbcc22b609
edidarenin <edidarenin@gmail.com>
2026-06-19 15:31:38 +0300

    Add DB connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "12345678";
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+
+                Class.forName("org.postgresql.Driver");
+
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Подключение к БД успешно!");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Драйвер PostgreSQL не найден!");
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.out.println("Ошибка подключения к БД!");
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Соединение с БД закрыто");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
